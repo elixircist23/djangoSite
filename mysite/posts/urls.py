dd30da5e35643226332800b4f45578b18030453d
tbone255 <taha.amir13971@gmail.com>
2016-01-20 20:22:36 -0500

    posts show at /posts

---
+++ mysite/posts/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+
+from .views import (
+	posts_list,
+	posts_create,
+	posts_detail,
+	posts_update,
+	posts_delete,
+	)
+
+urlpatterns = [
+	url(r'^$', "posts.views.posts_list"),
+    url(r'^create/$', "posts.views.posts_create"),
+    url(r'^detail/$', "posts.views.posts_detail"),
+    url(r'^update/$', "posts.views.posts_update"),
+    url(r'^delete/$', "posts.views.posts_delete"),
+	]
--- mysite/posts/views.py
+++ mysite/posts/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Post
 
 # Create your views here.
+def posts_create(request):
+	context = {
+		"title": "Create"
+	}
+	return render(request, "index.html", context)
+
+def posts_detail(request):
+	context = {
+		"title": "Detail"
+	}
+	return render(request, "index.html", context)
+
+def posts_list(request):
+	queryset = Post.objects.all()
+	context = {
+		"object_list": queryset ,
+		"title": "List"
+	}
+	return render(request, "index.html", context)
+
+def posts_update(request):
+	context = {
+		"title": "Update"
+	}
+	return render(request, "index.html", context)
+
+def posts_delete(request):
+	context = {
+		"title": "Delete"
+	}
+	return render(request, "index.html", context)
